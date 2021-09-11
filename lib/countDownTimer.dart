8d6ec55cf6a28c690b796ab7cb126027d4b0d240
KNkym <43508107+nisei1@users.noreply.github.com>
2021-09-11 17:45:24 +0900

    studyTime->restTimeへの遷移を追加

--- lib/countDownTimer.dart
+++ lib/countDownTimer.dart
@@ -9,33 +9,52 @@ class CountDownTimer extends StatefulWidget {
   _CountDownTimerState createState() => _CountDownTimerState();
 }
 
+//タイマーモードを切り替える用 列挙体
 enum _TimeModeState {
   study,
   rest,
 }
 
 class _CountDownTimerState extends State<CountDownTimer> {
-  String _timeModeStr = "STUDY TIME";
-  var _studytimer; //Timer型
+  String _timerModeStr = ""; //勉強時間か休憩時間か表示用変数
+  var _timer; //Timer型
+  _TimeModeState _mode = _TimeModeState.study;
+
+  DateTime _setTime = DateTime.utc(0, 0, 0); //使用する時間を代入するための変数
+  //勉強時間設定に使う変数
   int _studyTimeHour = 0; //勉強したいHour
   int _studyTimeMinute = 0; //勉強したいMinute
-  int _studyTimeSec = 2; //勉強したいSecond
-  DateTime _studytime = DateTime.utc(0, 0, 0);
+  int _studyTimeSec = 3; //勉強したいSecond
+  DateTime _studyTime = DateTime.utc(0, 0, 0); //勉強する時間
+  //休憩時間設定に使う変数
+  int _restTimeHour = 0; //休憩したいHour
+  int _restTimeMinute = 0; //休憩したいMinute
+  int _restTimeSec = 2; //休憩したいSecond
+  DateTime _restTime = DateTime.utc(0, 0, 0); //休憩する時間
+
   bool _isStartButtonDisable = false; //スタートボタンが無効かどうか
   bool _isStopButtonDisable = true; //ストップボタンが無効がどうか
 
   @override
   void initState() {
     //初期化処理
-    _studytime = _studytime.add(Duration(
+    _timerModeStr = "STUDY TIME";
+    _studyTime = _studyTime.add(Duration(
       hours: _studyTimeHour,
       minutes: _studyTimeMinute,
       seconds: _studyTimeSec,
     ));
+    _restTime = _restTime.add(Duration(
+      hours: _restTimeHour,
+      minutes: _restTimeMinute,
+      seconds: _restTimeSec,
+    ));
+
+    _setTime = _studyTime;
     super.initState();
   }
 
-  void _onTimer() {
+  void _onTimer(_TimeModeState _timerMode) {
     // タイマー起動
     setState(() {
       //状態を変更
@@ -43,19 +62,30 @@ class _CountDownTimerState extends State<CountDownTimer> {
       _isStopButtonDisable = false;
     });
 
-    _studytimer = Timer.periodic(
+    //タイマーモードによって使用する時間を切り替え,代入
+    if (_timerMode == _TimeModeState.study) {
+      setState(() {
+        _setTime = _studyTime;
+      });
+    } else {
+      setState(() {
+        _setTime = _restTime;
+      });
+    }
+
+    _timer = Timer.periodic(
       //タイマー開始
       Duration(seconds: 1), //一秒ごとに関数を実行
       (Timer timer) {
         setState(() {
           //状態を変更
-          if (_studytime == DateTime.utc(0, 0, 0)) {
+          if (_setTime == DateTime.utc(0, 0, 0)) {
             //時間が0になったら
             _offTimer(); //タイマーストップ
-            _resetTime(); //時間をリセット
+            _modeSwitch(_timerMode); //モード切り替え
             return; //関数終わり
           }
-          _studytime = _studytime.add(Duration(seconds: -1));
+          _setTime = _setTime.add(Duration(seconds: -1));
         });
       },
     );
@@ -67,20 +97,28 @@ class _CountDownTimerState extends State<CountDownTimer> {
     setState(() {
       _isStopButtonDisable = true;
       _isStartButtonDisable = false;
-      if (_studytimer != null && _studytimer.isActive)
-        _studytimer.cancel(); //タイマーストップ
+      if (_timer != null && _timer.isActive) _timer.cancel(); //タイマーストップ
     });
     return;
   }
 
-  void _resetTime() {
-    //時間をリセットする
-    _studytime = _studytime.add(Duration(
-      hours: _studyTimeHour,
-      minutes: _studyTimeMinute,
-      seconds: _studyTimeSec,
-    ));
-    return;
+  //受け取った_TimeModeStateを反転させる関数
+  void _modeSwitch(_TimeModeState _timerMode) {
+    if (_timerMode == _TimeModeState.study) {
+      setState(() {
+        _timerModeStr = "REST TIME";
+        _mode = _TimeModeState.rest;
+        _setTime = _restTime;
+      });
+      return;
+    } else {
+      setState(() {
+        _timerModeStr = "STUDY TIME";
+        _mode = _TimeModeState.study;
+        _setTime = _studyTime;
+      });
+      return;
+    }
   }
 
   @override
@@ -88,12 +126,12 @@ class _CountDownTimerState extends State<CountDownTimer> {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       Text(
         //勉強の時間なのか,休憩時間なのか表示
-        "$_timeModeStr",
+        "$_timerModeStr",
         style: Theme.of(context).textTheme.headline3,
       ),
       Text(
         //時間を表示
-        DateFormat.Hms().format(_studytime),
+        DateFormat.Hms().format(_setTime),
         style: Theme.of(context).textTheme.headline1,
       ),
       Row(
@@ -116,7 +154,7 @@ class _CountDownTimerState extends State<CountDownTimer> {
             onPressed: _isStartButtonDisable
                 ? null
                 : () {
-                    _onTimer();
+                    _onTimer(_mode);
                   },
           ),
         ],
